fe90a35ddc8d3348c1589f5e5ebfae6ff18b8037
sodizdaroglu <americanlaz@gmail.com>
2021-02-18 01:24:48 +0300

    heroku deploy

--- app/config/connection.js
+++ app/config/connection.js
@@ -2,8 +2,18 @@
 var Sequelize = require("sequelize");
 
 
-var sequelize = new Sequelize("datadb", "root", "a12345678", {
-  host: "localhost",
+// var sequelize = new Sequelize("datadb", "root", "a12345678", {
+//   host: "localhost",
+//   port: 3306,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
+var sequelize = new Sequelize("heroku_c6185c0d9cdb453", "bb2baa988e515f", "aca53adaa", {
+  host: "us-cdbr-east-03.cleardb.com",
   port: 3306,
   dialect: "mysql",
   pool: {
