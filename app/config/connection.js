ed3ea12984780536eb6257f4ee4fc5fa684aaaef
sodizdaroglu <americanlaz@gmail.com>
2021-02-17 19:09:42 +0300

    update

--- app/config/connection.js
+++ app/config/connection.js
@@ -2,8 +2,19 @@
 var Sequelize = require("sequelize");
 
 
-var sequelize = new Sequelize("datadb", "root", "a12345678", {
-  host: "localhost",
+// var sequelize = new Sequelize("datadb", "root", "a12345678", {
+//   host: "localhost",
+//   port: 3306,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
+
+var sequelize = new Sequelize("datadb", "root", "aca53adaa", {
+  host: "ca53adaa@us-cdbr-east-03.cleardb.com",
   port: 3306,
   dialect: "mysql",
   pool: {
@@ -13,5 +24,5 @@ var sequelize = new Sequelize("datadb", "root", "a12345678", {
   }
 });
 
-
 module.exports = sequelize;
+
